222277cd89eba54e6614c121d198f06d0c0f5772
TheMVProgrammer <franklin0415@hotmail.com>
2025-05-23 19:45:58 -0400

    feat(mongodb): add MongoDB connection setup and update dependencies
    - Introduced mongoose for MongoDB integration.
    - Added MONGODB_URI to .env.example for environment variable configuration.
    - Updated package.json and package-lock.json to include mongoose and related dependencies.

---
+++ lib/mongoose.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-var */
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined in the environment variables");
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, { dbName: "devflow" })
+      .then((result) => {
+        console.log("MongoDB connected: ", result.connection.host);
+        return result;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error: ", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
