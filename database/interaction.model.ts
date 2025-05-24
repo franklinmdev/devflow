c7446ef702c081bacb38f99c7ab33f9f654cca73
TheMVProgrammer <franklin0415@hotmail.com>
2025-05-23 23:54:02 -0400

    feat(database): add new models for answers, collections, interactions, questions, tags, and votes
    Introduced new Mongoose models for managing answers, collections, interactions, questions, tags, and votes in the database.
    Updated existing account and user models to include Document interface for better type safety.

---
+++ database/interaction.model.ts
@@ -0,0 +1,46 @@
+import { model, models, Schema, Types, Document } from "mongoose";
+
+const ACTION_ENUMS = [
+  "view",
+  "upvote",
+  "downvote",
+  "bookmark",
+  "post",
+  "edit",
+  "delete",
+  "search",
+] as const;
+
+const ACTION_TYPES_ENUMS = ["question", "answer"] as const;
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: (typeof ACTION_ENUMS)[number];
+  actionId: Types.ObjectId;
+  actionType: (typeof ACTION_TYPES_ENUMS)[number];
+}
+
+export interface IInteractionDoc extends IInteraction, Document {}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: {
+      type: String,
+      enum: ACTION_ENUMS,
+      required: true,
+    },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: {
+      type: String,
+      enum: ACTION_TYPES_ENUMS,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
---
+++ database/vote.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema, Types, Document } from "mongoose";
+
+export interface IVote {
+  author: Types.ObjectId;
+  actionId: Types.ObjectId;
+  type: "question" | "answer";
+  voteType: "upvote" | "downvote";
+}
+
+export interface IVoteDoc extends IVote, Document {}
+
+const VoteSchema = new Schema<IVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
+
+export default Vote;
