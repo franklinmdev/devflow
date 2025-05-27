69060b4f338ff234de02edf3dec9b799d00d1046
TheMVProgrammer <franklin0415@hotmail.com>
2025-05-27 19:51:45 -0400

    feat(action): ✨ add action handler for server-side processing
    Introduced a new action handler for server-side operations, including parameter validation using Zod and session management with NextAuth.
    - Added error handling for validation and unauthorized access.
    - Integrated database connection for data operations.
    - Updated package.json and package-lock.json to include the new "server-only" dependency.

---
+++ lib/handlers/action.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { Session } from "next-auth";
+import { ZodError, ZodSchema } from "zod";
+
+import { auth } from "@/auth";
+
+import { UnauthorizedError, ValidationError } from "../http-errors";
+import dbConnect from "../mongoose";
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodSchema<T>;
+  authorized?: boolean;
+};
+
+async function action<T>({
+  params,
+  schema,
+  authorized = false,
+}: ActionOptions<T>) {
+  if (params && schema) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>
+        );
+      } else {
+        return new Error("Schema validation failed");
+      }
+    }
+  }
+
+  let session: Session | null = null;
+
+  if (authorized) {
+    session = await auth();
+
+    if (!session) {
+      return new UnauthorizedError();
+    }
+  }
+
+  await dbConnect();
+
+  return { params, session };
+}
+
+export default action;
