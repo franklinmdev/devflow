d4cef248e14cba60de1d34443f05850a00a4fec9
TheMVProgrammer <franklin0415@hotmail.com>
2025-06-01 19:52:22 -0400

    feat(tag): ✨ add Tag page for displaying questions by tag
    - Created a new `Tag` page component to fetch and display questions associated with a specific tag.
    - Integrated `LocalSearch` for searching questions within the tag context.
    - Utilized `DataRenderer` to handle success, error, and empty states for question data.
    - Implemented dynamic rendering of `QuestionCard` components for each question retrieved.

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,51 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getTagQuestions } from "@/lib/actions/tag.action";
+
+const Tag = async ({ params, searchParams }: RouteParams) => {
+  const { id } = await params;
+  const { page, pageSize, query } = await searchParams;
+
+  const { success, data, error } = await getTagQuestions({
+    tagId: id,
+    page: Number(page || 1),
+    pageSize: Number(pageSize || 10),
+    query,
+  });
+
+  const { tag, questions } = data || {};
+
+  return (
+    <>
+      <section className="flex sm:flex-row flex-col-reverse justify-between sm:items-center gap-4 w-full">
+        <h1 className="text-dark100_light900 h1-bold">{tag?.name}</h1>
+      </section>
+
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAG(id)}
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <DataRenderer
+        success={success}
+        error={error}
+        data={questions}
+        empty={EMPTY_QUESTION}
+        render={(questions) => (
+          <div className="flex flex-col gap-6 mt-10 w-full">
+            {questions.map((question) => (
+              <QuestionCard key={question._id} question={question} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
+};
+
+export default Tag;
